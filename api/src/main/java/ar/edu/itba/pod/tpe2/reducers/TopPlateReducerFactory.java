9b528979ec1355526f1421e267b7bfbbfc9fa70e
santiagohirsch <shirsch@itba.edu.ar>
2024-06-08 19:56:25 -0300

    feat: TopPlateReducer
    Co-authored-by: Nicolas Suarez Durrels <nsuarezdurrels@itba.edu.ar>

---
+++ api/src/main/java/ar/edu/itba/pod/tpe2/reducers/TopPlateReducerFactory.java
@@ -0,0 +1,33 @@
+package ar.edu.itba.pod.tpe2.reducers;
+
+import ar.edu.itba.pod.tpe2.models.Pair;
+import com.hazelcast.mapreduce.Reducer;
+import com.hazelcast.mapreduce.ReducerFactory;
+
+@SuppressWarnings("deprecation")
+public class TopPlateReducerFactory implements ReducerFactory<Pair<String, String>, Integer, Integer> {
+
+    @Override
+    public Reducer<Integer, Integer> newReducer(Pair<String, String> stringStringPair) {
+        return null;
+    }
+
+    private static class TopPlateReducer extends Reducer<Integer, Integer> {
+        private int sum;
+
+        @Override
+        public void beginReduce() {
+            sum = 0;
+        }
+
+        @Override
+        public void reduce(Integer integer) {
+            sum += integer;
+        }
+
+        @Override
+        public Integer finalizeReduce() {
+            return sum;
+        }
+    }
+}
