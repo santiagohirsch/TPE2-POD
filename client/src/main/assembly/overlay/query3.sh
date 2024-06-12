bd4495ec60268a77579e2f8dbcae7c0f681cf0ed
santiagohirsch <shirsch@itba.edu.ar>
2024-06-12 12:18:12 -0300

    fix: class path on scripts

--- client/src/main/assembly/overlay/query3.sh
+++ client/src/main/assembly/overlay/query3.sh
@@ -4,7 +4,7 @@ PATH_TO_CODE_BASE=`pwd`
 
 #JAVA_OPTS="-Djava.rmi.server.codebase=file://$PATH_TO_CODE_BASE/lib/jars/rmi-params-client-1.0-SNAPSHOT.jar"
 
-MAIN_CLASS="ar.edu.itba.pod.tp2.client.Client"
+MAIN_CLASS="ar.edu.itba.pod.tpe2.client.Client"
 
 ARGS=("query3")
 
